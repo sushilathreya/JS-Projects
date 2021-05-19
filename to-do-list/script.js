f93fef78703cf91876b47e60348b1d44e1f4703a
Sushil Athreya <sushilathreya@gmail.com>
2021-05-19 20:06:52 +0530

    Added local storage

--- to-do-list/script.js
+++ to-do-list/script.js
@@ -3,31 +3,56 @@ const addNoteBtn = document.getElementById('add-note-btn');
 const noteUl = document.getElementById('notes');
 const searchBar = document.getElementById('search');
 
-let items = [];
+// let items = getNotes(), id;
 
+populateDOM();
 function addNote() {
     
     if(input.value!=''){
         const listItem = document.createElement('li');
-        listItem.setAttribute("id", input.value)
+        listItem.setAttribute("id", input.value);  
         listItem.innerHTML = `
             <div class = "li-div">
-                <input type = "checkbox" onclick = "this.parentElement.classList.toggle('checked') class="check-box"">
+                <input type = "checkbox" onclick = "this.parentElement.classList.toggle('checked')">
                 <p>${input.value}</p>
             </div>
-            <button class = "delete-btn" id = "delete-btn" onclick = "this.parentElement.parentElement.removeChild(${input.value})">
+            <button class = "delete-btn" id = "delete-btn" onclick = "this.parentElement.parentElement.removeChild(${input.value}), localStorage.removeItem('${input.value}')">
                 <i class = "fas fa-trash-alt"></i>
             </button>
         `;
         noteUl.appendChild(listItem);
-        items.push(input.value);
-
+        localStorage.setItem(`${input.value}`, input.value);
         input.value = '';
 
 } else {
     alert('Enter an item');
 }
 }
+
+
+function populateDOM () {
+    let keys = Object.keys(localStorage),
+    i = keys.length;
+    while(i--) {
+        let noteItem = document.createElement('li');
+        value = localStorage.getItem(keys[i]);
+        
+        noteItem.innerHTML = `
+        <div class = "li-div">
+            <input type = "checkbox" onclick = "this.parentElement.classList.toggle('checked')">
+            <p>${value}</p>
+        </div>
+        <button class = "delete-btn" id = "delete-btn" onclick = "this.parentElement.parentElement.removeChild(${value}), localStorage.removeItem('${value}')">
+            <i class = "fas fa-trash-alt"></i>
+        </button>
+    `;
+    noteUl.appendChild(noteItem);
+    }
+
+
+}
+
+
 
 // Search on user input
 function search() {
@@ -43,6 +68,15 @@ function search() {
 }
 
 
+function removeItem() {
+    console.log('remove');
+    let item = this.parentNode.parentNode;
+    let parent = this.parentNode;
+    parent.removeChild(item);
+
+    deleteBtn.removeEventListener('click', removeItem)
+}
+
 // Add note event listener
 addNoteBtn.addEventListener('click', addNote);
 
@@ -55,4 +89,5 @@ input.addEventListener('keyup', (e) => {
 searchBar.addEventListener('keyup', (e) => {
        search(); 
 });
+
 // 
